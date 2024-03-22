35d9407bc106ef495e51db819ac964cb6f048cec
Eduardo Juarez <eduardo.juarez@base22.com>
2024-03-22 00:46:06 -0600

    Add router to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import { Navigation } from "./components";
+import { Home, AboutUs, ContactUs } from "./pages";
 import { TaskList } from "./components";
 
 function App(): JSX.Element {
-  return <TaskList />;
+  return (
+    <>
+      <Navigation />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about-us' element={<AboutUs />} />
+        <Route path='/contact-us' element={<ContactUs />} />
+      </Routes>
+      <TaskList />
+    </>
+  );
 }
 
 export default App;
