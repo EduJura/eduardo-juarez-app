6f0446cc6a574757a0a88106043496f76d2dc65e
Eduardo Juarez <eduardo.juarez@base22.com>
2024-03-22 00:44:54 -0600

    Add pages

---
+++ src/pages/AboutUs.tsx
@@ -0,0 +1,5 @@
+function AboutUs(): JSX.Element {
+  return <h1>About Us</h1>;
+}
+
+export default AboutUs;
---
+++ src/pages/ContactUs.tsx
@@ -0,0 +1,5 @@
+function ContactUs(): JSX.Element {
+  return <h1>ContactUs</h1>;
+}
+
+export default ContactUs;
