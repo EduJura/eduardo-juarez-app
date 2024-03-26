04c205d0256aca36ae01f3f56bb2d5278661699b
Eduardo Juarez <eduardo.juarez@base22.com>
2024-03-26 15:32:35 -0600

    Add CustomLink to Navigation component

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,21 +1,26 @@
-import { Link } from "react-router-dom";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import "./Navigation.scss";
 
 function Navigation(): JSX.Element { 
   return (
     <nav>
       <ul>
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='/about-us'>About</Link>
-        </li>
-        <li>
-          <Link to='/contact-us'>Contact</Link>
-        </li>
+        <CustomLink url='/'>Home</CustomLink>
+        <CustomLink url='/about-us'>About Us</CustomLink>
+        <CustomLink url='/contact-us'>Contact Us</CustomLink>
       </ul>
     </nav>
   );
 }
+
+function CustomLink({children, ...props }): JSX.Element {
+  const resolvedPath = useResolvedPath(props.url);  
+  const isActive = useMatch({path: resolvedPath.pathname, end: true});
+  return (
+    <li className={isActive ? "navigation--active" : ""}>
+      <Link to={props.url}>{children}</Link>
+    </li>
+  );
+};
 
 export default Navigation;
