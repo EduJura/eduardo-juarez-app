ce57a0c43e15d98598c5c2636e97b69aa14f11d9
Eduardo Juarez <eduardo.juarez@base22.com>
2024-04-25 01:13:10 -0600

    Update index components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
+export { default as Header } from "./Header";
+export { default as Footer } from "./Footer";
 export { default as Navigation } from "./Navigation";
 export { default as Task } from "./Task";
 export { default as TaskList } from "./TaskList";
