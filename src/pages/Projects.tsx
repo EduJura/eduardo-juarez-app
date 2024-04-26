815c7e94ef6ce346cbb54248701931dbd49b86e9
Eduardo Juarez <eduardo.juarez@base22.com>
2024-04-26 17:05:27 -0600

    Add projects

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,8 +1,61 @@
+import { useState, useEffect } from "react";
+
 function Projects() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    const projectsData = getProjects();
+    projectsData.then((data) => setProjects(data.projects));
+  }, []);
+
+  async function getProjects(): Promise<any> {
+    const projectsURL = "./data/projects.json";
+    const response = await fetch(projectsURL);
+    return await response.json();
+  }
+
   return (
-    <div>
-      <h1>Projects</h1>
-    </div>
+    <>
+      <h1 className='text-2xl text-gray-900 font-[500] sm:text-6xl'>
+        Projects
+      </h1>
+      {/*-- TODO: Create  timeline component */}
+      <div className='timeline my-8'>
+        {projects.map((project: any, index: number) => (
+          <div key={index} className='timeline-item flex flex-row'>
+            <div className='timeline-marker flex flex-col items-center'>
+              <div className='timeline-marker-circle flex flex-col items-center justify-center rounded-full bg-slate-600 w-6 h-6 flex-shrink-0'>
+                <div className='rounded-full bg-emerald-400 w-2 h-2'></div>
+              </div>
+              <div
+                className={`timeline-marker-line dark:bg-slate-600 w-1 h-full ${
+                  index === projects.length - 1 ? "hidden" : ""
+                }`}></div>
+            </div>
+            <div className='timeline-content flex flex-col items-start pl-4 pb-8 hover:pl-8 hover:scale-105 duration-300 cursor-pointer'>
+              <h2 className='flex items-center text-xl font-medium mb-2'>
+                <span>{project.name}</span>
+                <span className='text-gray-500 text-sm ml-2'>
+                  ({project.startDate} - {project.endDate})
+                </span>
+              </h2>
+              <p className='text-lg text-gray-600'>{project.description}</p>
+              <div className='timeline-tags my-3'>
+                {project.technologies.map(
+                  (technology: string, index: number) => (
+                    <span
+                      key={index}
+                      className='technology text-xs rounded bg-slate-300 p-1 mr-2'>
+                      {technology}
+                    </span>
+                  )
+                )}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
