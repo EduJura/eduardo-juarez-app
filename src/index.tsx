f57d899464e3a1e6354e62fc4a2da740643ecaa4
Eduardo Juarez <eduardo.juarez@base22.com>
2024-04-29 15:03:40 -0600

    Upgrade create-react-app and its dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
-//import 'bootswatch/dist/lumen/bootstrap.min.css';
 import "./index.css";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
   <React.StrictMode>
     <BrowserRouter
       basename='/my-react-app-with-ts'>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
