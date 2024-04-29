599f0cea7ba88d55d9a983cf4804e0aadf24e834
Eduardo Juarez <eduardo.juarez@base22.com>
2024-04-29 13:09:52 -0600

    Add react toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { Header, Footer } from "./components";
 import { Home, Skills, Projects, Contact, ToDo } from "./pages";
+import { Toaster } from "react-hot-toast";
 
 function App(): JSX.Element {
   return (
     <>
       <Header />
-      <div className='container mx-auto max-w-screen-lg my-32'>
+      <div className='container mx-auto max-w-screen-lg px-2 py-8 md:py-32'>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/skills' element={<Skills />} />
@@ -14,6 +15,9 @@ function App(): JSX.Element {
           <Route path='/contact' element={<Contact />} />
           <Route path='/to-do' element={<ToDo />} />
         </Routes>
+      </div>
+      <div>
+        <Toaster reverseOrder={true} />
       </div>
       <Footer />
     </>
