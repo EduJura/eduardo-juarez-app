91fd4616450a15e545573e33dffe9c591d0e8b95
Eduardo Juarez <eduardo.juarez@base22.com>
2024-05-04 10:56:19 -0600

    Update projects with Timeline and FeedbackMessage components

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,5 @@ export { default as Footer } from "./Footer";
 export { default as Navigation } from "./Navigation";
 export { default as Task } from "./Task";
 export { default as TaskList } from "./TaskList";
+export { default as Timeline } from "./Timeline";
+export { default as FeedbackMessage } from "./FeedbackMessage";
