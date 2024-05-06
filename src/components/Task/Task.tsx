4a57df4dce4367db6aaa1aac5f42f52a8904c447
Eduardo Juarez <eduardo.juarez@base22.com>
2024-05-06 12:04:20 -0600

    Update Task compoonents look and feel

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,38 +1,40 @@
+import { FaTrashAlt } from "react-icons/fa";
+import { ImCheckboxChecked, ImCheckboxUnchecked } from "react-icons/im";
 import "./Task.scss";
 
 function Task({ task, dispatch }): JSX.Element {
   return (
-    <div className='card card-body mt-2'>
-      <div className='row'>
-        <div className='col-md-9'>
-          <h2 className={task.done ? "task--done" : ""}>
-            {task.name} - {task.id}
-          </h2>
-        </div>
-        <div className='col-md-3'>
-          <button
-            className='btn btn-primary ml-3 mr-1'
-            data-testid='change-task-button'
-            onClick={(e) => {
-              dispatch({
-                type: "change_task",
-                taskId: task.id,
-              });
-            }}>
-            {task.done ? "✓" : "𝘟"}
-          </button>
-          <button
-            className='btn btn-warning'
-            data-testid='remove-task-button'
-            onClick={() => {
-              dispatch({
-                type: "remove_task",
-                taskId: task.id,
-              });
-            }}>
-            🗑
-          </button>
-        </div>
+    <div className='task text-2xl flex flex-row justify-between my-4'>
+      <div className={`text-white ${task.done ? "task--done" : ""}`}>
+        {task.name} - {task.id}
+      </div>
+      <div className=''>
+        <button
+          className='btn btn-primary ml-3 mr-2'
+          data-testid='change-task-button'
+          onClick={(e) => {
+            dispatch({
+              type: "change_task",
+              taskId: task.id,
+            });
+          }}>
+          {task.done ? (
+            <ImCheckboxChecked className='text-green-300 hover:scale-110 duration-300' />
+          ) : (
+            <ImCheckboxUnchecked className='text-red-300 hover:scale-110 duration-300' />
+          )}
+        </button>
+        <button
+          className='btn btn-warning'
+          data-testid='remove-task-button'
+          onClick={() => {
+            dispatch({
+              type: "remove_task",
+              taskId: task.id,
+            });
+          }}>
+          <FaTrashAlt className='text-slate-300 hover:scale-110 duration-300' />
+        </button>
       </div>
     </div>
   );
--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -3,7 +3,7 @@ import { ITask } from "../../global/interfaces";
 import { tasksReducer } from "../../global/reducers";
 import { Task } from "../../components";
 
-function TaskList(): JSX.Element { 
+function TaskList(): JSX.Element {
   const [tasks, dispatch] = useReducer(tasksReducer, []);
   const [idx, setIdx] = useState(0);
   const taskInput = useRef<HTMLInputElement>(null);
@@ -20,33 +20,29 @@ function TaskList(): JSX.Element {
 
   return (
     <>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-md-6 offset-md-3'>
-            <div className='card mt-4'>
-              <div className='card-body'>
-                <form>
-                  <input
-                    className='form-control'
-                    data-testid='task-input'
-                    type='text'
-                    autoFocus
-                    ref={taskInput}
-                  />
-                  <button
-                    className='btn btn-success btn-block mt-3'
-                    data-testid='save-task-button'
-                    onClick={handleSaveClick}>
-                    Save
-                  </button>
-                </form>
-              </div>
-            </div>
-            {tasks.map((task: ITask, index: number) => (
-              <Task task={task} dispatch={dispatch} key={index} />
-            ))}
-          </div>
-        </div>
+      <div
+        className='tasklist dark:bg-slate-600 rounded-lg shadow-md
+            shadow-emerald-400 md:w-[50%] m-auto mt-8 p-4'>
+        <form className='flex flex-row items-stretch mb-12'>
+          <input
+            className='rounded h-12 p-2 w-full mr-2'
+            name='task-input'
+            placeholder='Type your name'
+            data-testid='task-input'
+            type='text'
+            autoFocus
+            ref={taskInput}
+          />
+          <button
+            className='bg-emerald-400 text-white p-2 rounded-md cursor-pointer hover:scale-110 duration-300'
+            data-testid='save-task-button'
+            onClick={handleSaveClick}>
+            Save
+          </button>
+        </form>
+        {tasks.map((task: ITask, index: number) => (
+          <Task task={task} dispatch={dispatch} key={index} />
+        ))}
       </div>
     </>
   );
