6e02d76696026d29e0ab3088448e8cb937a26ee1
Eduardo Juarez <eduardo.juarez@base22.com>
2024-05-15 14:18:45 -0600

    Add shell script to test the ci/cd workflow action locally

---
+++ scripts/act-ci-cd-pipeline-test.sh
@@ -0,0 +1,16 @@
+# To test the Github actions from local environment it's needed to install act from nektos
+# List jobs to be executed
+echo "Listing jobs to be executed..."
+act --graph
+# Create a temporal directory to perform to simulate uploading/downloading artifacts
+mkdir -p ./tmp/artifacts
+echo "Created temporal artifacts directory"
+# Execute the workflow by setting the artifact-server-path, the workflow path and the ACT_LOCAL environment variable
+# Note: Create a Github Personal Access Token and set it as the GITHUB_TOKEN environment variable.
+# The GITHUB_TOKEN (Personal Access Token) environment variable is required to authenticate with Github.
+echo "Executing workflow..."
+act --artifact-server-path tmp/artifacts -W .github/workflows/ci-cd.yml --env ACT_LOCAL=true -s GITHUB_TOKEN=$GITHUB_TOKEN
+# Cleanup the temporal directory
+echo "Cleaning up temporal artifacts directory..."
+rm -rf ./tmp
+echo "Complete it successfully!"
