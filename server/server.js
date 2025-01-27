35c8ab52f5fefffab6ab426ed5d258519cacb3ff
Eduardo Juárez <j_aton_7@hotmail.com>
2025-01-27 13:52:53 -0600

    Update root route and cors

--- server/server.js
+++ server/server.js
@@ -1,24 +1,36 @@
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./db/connection");
+const path = require("path");
 //const Project = require('./db/prjectSchema');
 const projectRoutes = require("./routes/projectRoutes");
 const PORT = process.env.PORT || 3000;
-const apiURL = "/api/v1/projects";
+const apiURL = "/api/v1";
 const app = express();
 
 // Connect to MongoDB
 connectDB();
 // Create Express application
-app.use(cors());
+// Specify allowed origins (CORS)
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:3001"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    //allowedHeaders: ['Content-Type', 'Authorization'], // If you need Headers
+    //credentials: true, // If you need to send cookies or credentials
+  })
+);
+
 // Middleware
 app.use(express.json());
+// Static file serving (for index.html and other static files)
+app.use(express.static(path.join(__dirname, "public")));
 // Routes
 app.use(apiURL, projectRoutes);
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Something went wrong!');
+  res.status(500).send("Something went wrong!");
 });
 // Start server
 app.listen(PORT, () => {
